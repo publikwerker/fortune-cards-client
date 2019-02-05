1210608cce9b8572c425b17f8344b999d242dc50
Publikwerker <jason.michael.hoffman@gmail.com>
2019-02-05 09:17:41 -0800

    connected the back to the front

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { API_BASE_URL } from './config.js';
 import Query from './components/query.js';
 import Header from './components/header.js';
 import Spread from './components/spread.js';
@@ -9,7 +10,31 @@ export default class App extends Component {
     super(props);
     this.state = {
     };
-  };
+  }
+
+componentDidMount() {
+  this.loadDeck();
+}
+
+loadDeck(){
+  return fetch(`${API_BASE_URL}/tarotDeck`)
+    .then(res => {
+      if (!res.ok){
+        return Promise.reject(res.statusText);
+      }
+      return res.json();
+    })
+    .then(tarotDeck =>
+      this.setState({
+        deck: tarotDeck
+      })
+    )
+    .catch(err =>
+      this.setState({
+        error: 'could not load deck'
+      })
+    );
+}
   
   render() {
 
