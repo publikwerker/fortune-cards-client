69f54aaf8abe69b7528e0004747e3ade6bc99fa4
Publikwerker <jason.michael.hoffman@gmail.com>
2019-02-05 14:11:09 -0800

    cards rotate in accordance with direction

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ import './card.css';
 
 export function CardDescriptions(props) {
   return (
-    <div className="cardDescription" id={props.id}>
+    <div className="card-description" id={props.id}>
       <h3>{props.id}</h3>
       <h4>{props.name}</h4>
       {props.desc}
@@ -13,10 +13,11 @@ export function CardDescriptions(props) {
 };
 
 export function CardImages(props){
-  console.log(props.img);
+  console.log(props.direction);
+  let classes = `card-image rotate${props.direction}`
   return (
     <div>
-      <img src={props.img} alt={props.name} />
+      <img className={classes} src={props.img} alt={props.name} />
     </div>
   )
 }
