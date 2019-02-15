838a4d87fcdb893da0654e7d738d218ec0ac9f46
Publikwerker <jason.michael.hoffman@gmail.com>
2019-02-15 13:44:01 -0800

    adjusted styling slightly for larger screens

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,6 @@ import './footer.css';
 export default function Footer(){
 return (
   <div className="footer">
-  <p className="copy-info">All images and text copyright 2019 Publikwerks, LLC.</p>
+  <p className="copy-info">All images and text &copy; 2019 Publikwerks, LLC.</p>
   </div>
 )};
