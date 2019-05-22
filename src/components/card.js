173a95e8b9cb37d102ccd5e260372f699cf32646
Publikwerker <hello@publikwerks.com>
2019-05-22 11:50:14 -0700

    added facing description to each card in the spread

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 export function CardDescriptions(props) {
+  let facingText = '';
+  if (props.facing){
+    switch (props.facing) {
+      case 0:
+        facingText = 'Facing upward.';
+        break;
+      case 1:
+        facingText = 'Facing to the right.';
+        break;
+      case 2:
+        facingText = 'Facing downward.';
+        break;
+      case 3:
+        facingText = 'Facing to the left.';
+        break;
+      default:
+        break;
+    }
+  }
   return (
     <div className="card-description-box" id={props.id}>
       <h3>{props.id}</h3>
       <h4>{props.name}</h4>
+      <h5>{facingText}</h5>
       <p className="desc-text">{props.desc}</p>
     </div>
   );
