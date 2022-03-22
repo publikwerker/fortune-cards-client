0441b438f5c81ea1ff0277ba27d89be8da079283
Publikwerker <jason.michael.hoffman@gmail.com>
2022-03-21 20:46:32 -0700

    added google translate button to footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,9 @@ import React from 'react';
 export default function Footer(){
 return (
   <footer className="footer">
+    <p className="footer__copy-info">Thank you for using this site!</p>
+    <p className="footer__copy-info">You can translate the contents of this page using the Google Translate button below.</p>
+    <div id='google_translate_element'></div>
     <p className="footer__copy-info">All images and text &copy; 2019 Publikwerks, LLC.</p>
   </footer>
 )};
